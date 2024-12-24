5376c7d2b1cbabd30306bfff00bfe5a1407b85c7
Utkarsh Verma <vermautkarsh653@gmail.com>
2024-12-24 10:10:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ const upload = multer({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'tmp')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
